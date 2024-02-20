c4361d687e47b3437bb0ea9e960174b10fa23bf2
Đạt Ma <thanhdat@zim.vn>
2024-02-20 21:28:17 +0700

    setup > eslint initial

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'standard',
+    'eslint:recommended'
+  ],
+
+  rules: {
+    'no-dupe-keys': ['error'],
+    'no-unused-vars': ['warn'],
+    'no-use-before-define': ['error'],
+    'no-empty': 'warn'
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../../../../../../vite.svg'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
+
+  const obj = {
+    a: 3
+  }
+  const a = () => {
+
+  }
+
+  function b () {
+    console.log('cc')
+  }
+  console.log(obj)
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
